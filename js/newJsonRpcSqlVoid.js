2ecbf429089313795c635e51e38795842efd41e7
Rob Azinger <razinger@techstyle.com>
2022-03-16 17:43:25 -0400

    new snipper

--- js/newJsonRpcSqlVoid.js
+++ js/newJsonRpcSqlVoid.js
@@ -10,13 +10,13 @@ function _getPallet(self, pallet, warehouseId, response, done) {
     sql.input('label', self._db.types.VarChar, pallet);
     sql.input('warehouse_id', self._db.types.Int, warehouseId);
 
-    sql.execute('dbo.pr_pallet_by_label_sel', (err, recordsets) => {
+    sql.execute('dbo.pr_pallet_ins', (err, recordsets) => {
         if (err) {
             response.addError({
                 code: 500,
                 message: 'error executing database query',
                 data: {
-                    _proc: 'dbo.pr_pallet_by_label_sel',
+                    _proc: 'dbo.pr_pallet_ins',
                     err: err
                 }
             });
@@ -24,24 +24,6 @@ function _getPallet(self, pallet, warehouseId, response, done) {
             return done('fail');
         }
 
-        let result = {};
-
-        if (!recordsets[0].length) {
-            response.addError(500, `${pallet} is not valid.`);
-            return done('fail');
-        }
-
-        result = {
-            palletId: recordsets[0][0].pallet_id,
-            palletCode: recordsets[0][0].pallet_code,
-            label: recordsets[0][0].label,
-            warehouseId: recordsets[0][0].warehouse_id,
-            statusCodeId: recordsets[0][0].status_code_id,
-            datetimeAdded: recordsets[0][0].datetime_added,
-            datetimeModified: recordsets[0][0].datetime_modified,
-            outboundPalletId: recordsets[0][0].outbound_pallet_id
-        };
-
-        done(null, result);
+        done(null);
     });
 }
